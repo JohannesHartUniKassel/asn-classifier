9e1b66f32708256dd8ae7a857b26beab0f406567
JohannesHartUniKassel <uk090866@student.uni-kassel.de>
2025-11-03 09:57:49 +0000

    savegame

--- preprocessing/data/clickhouse/import.sh
+++ preprocessing/data/clickhouse/import.sh
@@ -2,7 +2,7 @@
 
 python fix_sql.py ./ch_dump/*.sql
 
-clickhouse-client --queries-file ch_dump/*.sql
+click --queries-file ch_dump/*.sql
 
 for file in ch_dump/*.native; do
     # Extrahiere den Tabellennamen aus dem Dateinamen (ohne .native)
